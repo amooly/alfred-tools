de5d2663628386993c7aff9449a8d5ec1e8bbbe4
墨黎 <haibin.whb@alibaba-inc.com>
2018-12-15 23:15:56 +0800

    bugfix：reimplement hash function.

--- hash.go
+++ hash.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"log"
 	"os"
 	"strconv"
 )
 
+// java hashCode function implemented in golang
 func hash() (string, error) {
 	sorStr := string([]rune(os.Args[2]))
 
-	tgtStr := 0
+	target := 0
 	for i := 0; i < len(sorStr); i++ {
-		tgtStr = 31*tgtStr + int(sorStr[i])
+		target = 31*target + int(sorStr[i])
+		target = target & 0xFFFFFFFF
 	}
-	log.Print(tgtStr)
-	return strconv.Itoa(tgtStr), nil
+	if target&0x80000000 != 0 {
+		target = (target ^ 0xFFFFFFFF) + 1
+	}
+
+	return strconv.Itoa(target), nil
 }
